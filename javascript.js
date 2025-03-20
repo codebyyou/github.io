ef487a3708eac3dc9ea308d4d2e9c1b7f23e3f72
codebyyou <dpm.email1368@gmail.com>
2025-03-20 15:58:09 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 let email_input = setTimeout(function() {prompt("Would you like to subscribe to receive emails? \nIf so, enter your email address here: ",""); } , 5000);
-
 let clicks = 0;
 
 document.getElementById("link").addEventListener("pointerdown", () => {
@@ -29,3 +28,25 @@ let style2 = document.getElementById("changestyle2").addEventListener("pointerdo
 let style3 = document.getElementById("changestyle3").addEventListener("pointerdown", () => {
     style3.onclick = document.getElementById("pagestyle").setAttribute("href", "style3.css");
 })
+
+const username = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const message = document.getElementById('message').value;
+const send = document.getElementById('send');
+
+send.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    if (username === "" || email === "" || message === "") {
+        alert("Please fill out all fields before submitting.");
+        // send.disabled = true;
+    }
+    else{
+        // send.disabled = false;
+        username, email, message = "";
+    }
+
+})
+
+
