3d283c65de956427ca70f83dc49e9c7bd9f14096
codebyyou <dpm.email1368@gmail.com>
2025-03-21 16:58:21 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
+emailjs.init("NhNnJxHaGwpH77-LU");
+
 let email_input = setTimeout(function() {prompt("Would you like to subscribe to receive emails? \nIf so, enter your email address here: ",""); } , 5000);
 let clicks = 0;
 
-
-let alist  =document.querySelectorAll("a");
-console.log(alist)
+let alist = document.querySelectorAll("a");
 
 for (let i = 0; i < alist.length; i ++) {
 
@@ -27,15 +27,33 @@ let style3 = document.getElementById("changestyle3").addEventListener("pointerdo
     style3.onclick = document.getElementById("pagestyle").setAttribute("href", "style3.css");
 })
 
-
-function showAlert(event){
-    
+document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert("Email sent successfully!");
-    document.getElementById('name').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('message').value = "";
-
-}
 
 
+    let templateParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+      };
+
+    let name = document.getElementById('name').value;
+
+    const nameRegex = /^[A-Za-z]+$/;
+
+    if (!nameRegex.test(name)) {
+        alert('Please enter a valid name containing only alphabetic characters.');
+    } 
+    else {
+        emailjs.send('service_oblehcv', 'template_a40h6q9', templateParams)
+            .then(function(response) {
+                alert('Email sent successfully!');
+            }, function(error) {
+                alert('Failed to send email: ' + error.text);
+            });
+            document.getElementById('name').value = "";
+            document.getElementById('email').value = "";
+            document.getElementById('message').value = "";
+            
+    }
+})
