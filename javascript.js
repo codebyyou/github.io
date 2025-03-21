5f1068615578dd08ca2806eaa7bfaa26cd2e57bc
codebyyou <dpm.email1368@gmail.com>
2025-03-21 22:10:30 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,8 +2,10 @@ emailjs.init("NhNnJxHaGwpH77-LU");
 
 let email_input = setTimeout(function() {prompt("Would you like to subscribe to receive emails? \nIf so, enter your email address here: ",""); } , 5000);
 let clicks = 0;
+let image_cycle = 0;
 
 let alist = document.querySelectorAll("a");
+let imglist = document.querySelectorAll("img");
 
 for (let i = 0; i < alist.length; i ++) {
 
@@ -11,25 +13,63 @@ for (let i = 0; i < alist.length; i ++) {
 
         clicks++
 
-        document.getElementById("linkCounter").innerHTML = `Number of clicks on this page ==> ${clicks}`;
+        document.getElementById("linkCounter").innerHTML = `Click<br>Counter:<br>${clicks}`;
 
     })
 
 }
 
+setInterval("changeImage()",20000)
+
+for (let i = 2; i < imglist.length; i ++) {
+
+    imglist[i].classList.add("animateout");
+
+}
+
+function changeImage() {
+
+    image_cycle = image_cycle == 0 ? 1 : 0;
+
+    for (let i = 2; i < imglist.length; i ++) {
+
+        imglist[i].classList.remove("animateout");
+        console.log(imglist)
+
+        setTimeout(() => {
+
+            if (imglist[i].id == "html") {
+                document.getElementById("html").src = `HTML${image_cycle}.png`;
+            }
+
+            if (imglist[i].id == "css") {
+                document.getElementById("css").src = `CSS${image_cycle}.png`;
+            }
+
+            if (imglist[i].id == "js") {
+                document.getElementById("js").src = `JS${image_cycle}.png`;
+            }
+
+            imglist[i].classList.add("animateout");
+            console.log(imglist)
+
+        }, 1000)
+    }
+}
+
 let style1 = document.getElementById("changestyle1").addEventListener("pointerdown", () => {
-    style1.onclick = document.getElementById("pagestyle").setAttribute("href", "style1.css");
+    style1 = document.getElementById("pagestyle").setAttribute("href", "style1.css");
 })
 let style2 = document.getElementById("changestyle2").addEventListener("pointerdown", () => {
-    style2.onclick = document.getElementById("pagestyle").setAttribute("href", "style2.css");
+    style2 = document.getElementById("pagestyle").setAttribute("href", "style2.css");
 })
 let style3 = document.getElementById("changestyle3").addEventListener("pointerdown", () => {
-    style3.onclick = document.getElementById("pagestyle").setAttribute("href", "style3.css");
+    style3 = document.getElementById("pagestyle").setAttribute("href", "style3.css");
 })
 
 document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault();
 
+    event.preventDefault();
 
     let templateParams = {
         name: document.getElementById('name').value,
